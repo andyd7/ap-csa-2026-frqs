7b89d7e77a3bc4c7dae66e2f7388c16110d0cb45
andyd7 <andyd76@nycstudents.net>
2026-05-19 11:36:58 -0400

    Update Solution.java

--- question-1/Solution.java
+++ question-1/Solution.java
@@ -1,3 +1,20 @@
 public class solution{
-
+ private String username;
+  public Account(String requestedName){
+  username = requestedName;
+    int k = 1;
+    if(isAvailable(username)){
+     username = requestedName;
+    } else if(!isAvailable(username)){
+      while(k>0){
+        String newName = username + k;
+        if(isAvailable(newName)){
+          username = newName;
+          break;
+        } else{
+         k++;
+        }
+      }
+    }
+  }
 }
