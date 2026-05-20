b7346fe4461a41811c35433ed913e119e53f86f4
andyd7 <andyd76@nycstudents.net>
2026-05-20 11:44:12 -0400

    Update Solution.java

--- question-2/Solution.java
+++ question-2/Solution.java
@@ -1,3 +1,18 @@
 public class Solution{
+   public class Bottle{
+    private double capacity;
+     private double waters;
+     public Bottle(double fill){
+      capacity = fill;
+       waters = fill;
+     }
 
+     public double updateAmount(double newFill){
+     waters -= newFill;
+       if(waters < 0.25 * capacity){
+         waters = capacity;
+       }
+       return waters;
+     }
+   }
 }
