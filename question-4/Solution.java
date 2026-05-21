cbadd39d5f11dc551a01698d8316286b33d76b00
andyd7 <andyd76@nycstudents.net>
2026-05-21 11:46:11 -0400

    Update Solution.java

--- question-4/Solution.java
+++ question-4/Solution.java
@@ -1,2 +1,18 @@
 public class Solution{
+  private int r;
+  public int getPointsForRow(int targetRow){
+   r = targetRow;
+    int sum = 0;
+    int k = 0;
+    for(int c = 0; c < board[0].length ; c++){
+      sum += board[r][c].getPoints();
+      if(board[r][c].getColor().equals(board[r][c-1].getColor())){
+        k++;
+      }
+    }
+    if(k == 5){
+      sum *= 2;
+    }
+    return sum;
+  }
 }
