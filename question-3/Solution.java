8079a352262d79ec848bce164bd838f7a54918db
andyd7 <andyd76@nycstudents.net>
2026-05-22 11:24:27 -0400

    Update Solution.java

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,3 +1,17 @@
 public class Solution{
-
+ public int moreHistoryThanMathAbsences(ArrayList<CourseRecord> historyList, ArrayList <CourseRecord> mathList){
+  int count = 0;
+   for(CourseRecord historyRecord : historyList){
+     String targetID = historyRecord.getStudentID();
+      for(CourseRecord mathRecord : mathList){
+        if(mathRecord.getStudentID().equals(targetID){
+          if(historyRecord.getAbsences() > mathRecord.getAbsences()){
+            count++
+          }
+          break;
+        }
+      }
+   }
+   return count;
+ }
 }
